dc2364718cc6877f25179aa77888d794f37c5f3d
Karan Sambhaji Babar <118063735+karanbabars@users.noreply.github.com>
2024-08-20 21:52:04 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-echo "Enter the number -"
+echo "Enter the number "
 read n
 
 
 i=1
 while [ $i -le 10 ]
 do
-res=$(($i * $n))
-
-echo "$n * $i = $res"
-((i++))
+  echo $n=$n*$i=$(($n*$i))
+  ((i++))
 done
